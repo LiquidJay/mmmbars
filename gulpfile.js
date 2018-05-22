e8255958c6b3fb21528822a14042351b06b126cf
Jason Simmons <jason_m_simmons@yahoo.com>
2018-05-21 20:19:40 -0700

    add gulp sass ... need to get src working

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('sass', () =>
+    sass('styles/*.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('../styles/css'))
+);
+
+
+gulp.task('default', [ 'sass']);
